bce1b81e8d5cd1ff07074fee10bbeb92ec7d799d
Marx Rocha <165192193+marxrocha205@users.noreply.github.com>
2026-01-23 08:12:47 -0300

    Update decorators.py

--- sinistro_dash/dashboard/decorators.py
+++ sinistro_dash/dashboard/decorators.py
@@ -3,10 +3,23 @@ from django.shortcuts import redirect
 
 
 def api_login_required(view_func):
+
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        if not request.session.get("api_token"):
+
+        token = request.session.get("api_token")
+
+        print("🛡 DECORATOR api_login_required")
+        print("➡️ PATH:", request.path)
+        print("➡️ METHOD:", request.method)
+        print("➡️ SESSION TOKEN:", token)
+
+        if not token:
+            print("🚫 SEM TOKEN NA SESSÃO — REDIRECIONANDO LOGIN")
             return redirect("dashboard:login")
+
+        print("✅ TOKEN OK — ACESSO PERMITIDO")
+
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view
